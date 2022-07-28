00df88a0b716973a09fc94c7a98772dff165ec64
Khun Tun(Jus Tin) <jajahuge890@gmail.com>
2022-07-28 16:18:19 +0630

    evening

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// const signInFormsTag = document.querySelector(".btn");
+// // console.log(signInFormsTag);
+// const mainTwoTag = document.querySelector(".mainTwo");
+
+
+// signInFormsTag.addEventListener("click", () => {
+//         console.log("hiiiii",signInFormsTag);
+
+//         // if (signInFormsTag) {
+//         //         mainTwoTag.style.display = "block";
+//         // }else {
+
+//         // }
+// });
